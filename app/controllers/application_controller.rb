983b904a3b82f718a594d8235941dc4c47fe0346
Gopinath Sivakumar <gsivakumar@caesars.com>
2025-11-16 14:49:45 +0530

    chore: inital alive and no route setup is done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    
+    def alive
+        render json: { current_time: Time.now }
+    end
+
+    def route_not_found
+        render json: { error: "Route not found" }, status: :not_found
+    end
 end
