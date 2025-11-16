f10ae8991ad0756de266e926916e44dbac24d835
Gopinath Sivakumar <gsivakumar@caesars.com>
2025-11-16 15:27:18 +0530

    refactor: code alignment did in auth flow

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::AuthController < Api::V1::BaseController
     skip_before_action :authorize_request, only: [:signup, :login]
-  
+
     def signup
         user = User.new(user_params)
         user.status = 1 # Set user as active on signup
         if user.save
-        render json: { message: I18n.t('auth.signup_success') }, status: :created
+            render json: { message: I18n.t('auth.signup_success') }, status: :created
         else
-        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
         end
     end
 
@@ -16,16 +16,16 @@ class Api::V1::AuthController < Api::V1::BaseController
         password = params[:password].to_s
 
         if login_param.blank? || password.blank?
-        return render json: { error: I18n.t('auth.login_required') }, status: :bad_request
+            return render json: { error: I18n.t('auth.login_required') }, status: :bad_request
         end
 
         user = User.find_by("lower(email) = ?", login_param.downcase) || User.find_by(phone: login_param)
         if user&.authenticate(password)
-        token = JsonWebToken.encode(user_id: user.id)
-        user.mark_login! if user.respond_to?(:mark_login!)
-        render json: { message: I18n.t('auth.login_success'), token: token }, status: :ok
+            token = JsonWebToken.encode(user_id: user.id)
+            user.mark_login! if user.respond_to?(:mark_login!)
+            render json: { message: I18n.t('auth.login_success'), token: token }, status: :ok
         else
-        render json: { error: I18n.t('auth.invalid_credentials') }, status: :unauthorized
+            render json: { error: I18n.t('auth.invalid_credentials') }, status: :unauthorized
         end
     end
 
@@ -38,12 +38,12 @@ class Api::V1::AuthController < Api::V1::BaseController
 
     def user_params
         params.require(:user).permit(
-        :email,
-        :first_name,
-        :last_name,
-        :phone,
-        :password,
-        :password_confirmation
+            :email,
+            :first_name,
+            :last_name,
+            :phone,
+            :password,
+            :password_confirmation
         )
     end
 end
