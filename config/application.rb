4bfce4b1a7c7ac21f1bb5d04064903a8e08e876f
Gopinath Sivakumar <gsivakumar@caesars.com>
2025-11-16 15:07:39 +0530

    feat: configuration request_id logging

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module RagSystem
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.1
 
+    ## configuration request_id logging
+    config.log_tags = [:request_id]
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
