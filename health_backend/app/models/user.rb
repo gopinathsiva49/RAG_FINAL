d951418ce914f383c63e7217e05f0c24fd28c998
Gopinath Sivakumar <gsivakumar@caesars.com>
2025-11-18 21:44:35 +0530

    refactor: numberic removed

--- health_backend/app/models/user.rb
+++ health_backend/app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
     validates :last_name, presence: true
     validates :email, presence: true, uniqueness: true,
                         format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :phone, presence: true, uniqueness: true,
-                        length: { in: 10..15 },
-                        format: { with: /\A[0-9]+\z/, message: "must be numeric" }
+    validates :phone, presence: true, uniqueness: true
+
     def mark_login!
         update!(
             num_logins: num_logins + 1,
